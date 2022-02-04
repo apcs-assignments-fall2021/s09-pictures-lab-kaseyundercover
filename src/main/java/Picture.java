75746b15135abefc322d28aa1852e18cff080aed
kaseyk9 <23kkazliner@athenian.org>
2022-02-04 14:32:51 -0800

    Finished

--- src/main/java/Picture.java
+++ src/main/java/Picture.java
@@ -104,36 +104,80 @@ public class Picture extends SimplePicture
     // Using the zeroBlue method as a starting point, write the method keepOnlyBlue that
     // will keep **only** the blue values, that is, it will set the red and green values to zero
     public void keepOnlyBlue() {
-        // YOUR CODE HERE
+        Pixel[][] pixels = this.getPixels2D();
+        for (int row = 0; row < pixels.length; row++) {
+            for (int col = 0; col < pixels[0].length; col++) {
+                Pixel pix = pixels[row][col];
+                pix.setRed(0);
+                pix.setGreen(0);
+            }
+        }
     }
+
 
     // Write the negate method to negate all the pixels in a picture. To negate a picture, set the red
     // value to 255 minus the current red value, the green value to 255 minus the current green value
     // and the blue value to 255 minus the current blue value.
     public void negate() {
-        // YOUR CODE HERE
+        Pixel[][] pixels = this.getPixels2D();
+        for (int row = 0; row < pixels.length; row++) {
+            for (int col = 0; col < pixels[0].length; col++) {
+                Pixel pix = pixels[row][col];
+                pix.setRed(255-pix.getRed());
+                pix.setGreen(255-pix.getGreen());
+                pix.setBlue(255-pix.getBlue());
+            }
+        }
     }
 
     // Write the grayscale method to turn the picture into shades of gray. Set the red, green, and
     // blue values to the average of the current red, green, and blue values (add all three values and
     // divide by 3).
     public void grayscale() {
-        // YOUR CODE HERE
+        Pixel[][] pixels = this.getPixels2D();
+        for (int row = 0; row < pixels.length; row++) {
+            for (int col = 0; col < pixels[0].length; col++) {
+                Pixel pix = pixels[row][col];
+                int x = pix.getRed() + pix.getBlue() + pix.getGreen();
+                pix.setRed(x/3);
+                pix.setBlue(x/3);
+                pix.setGreen(x/3);
+            }
+        }
     }
+
 
     // Write the mirrorCopy method which mirrors and copies the left side of the image
     // onto the right side of the image
     // Note: you should set the colors values of the pixel you are changing with the
     // setter methods rather than trying to copy the actual pixel
     public void mirrorCopy() {
-        // YOUR CODE HERE
+        Pixel[][] pixels = this.getPixels2D();
+        for (int row = 0; row < pixels.length; row++) {
+            for (int col = 0; col < pixels[0].length/2; col++) {
+                Pixel pix = pixels[row][col];
+                Pixel pix2 = pixels[row][pixels[col].length-1-col];
+                pix2.setRed(pix.getRed());
+                pix2.setGreen(pix.getGreen());
+                pix2.setBlue(pix.getBlue());
+
+            }
+        }
     }
 
     // Bonus — Explore the "water.jpg" picture in the images folder. Write a method
     // fixUnderwater() to modify the pixel colors to make the fish easier to see
     public void fixUnderwater() {
-        // YOUR CODE HERE
-    }
+        Pixel[][] pixels = this.getPixels2D();
+        for (int row = 0; row < pixels.length; row++) {
+            for (int col = 0; col < pixels[0].length; col++) {
+                Pixel pix = pixels[row][col];
+                if (pix.getRed()<20){
+                    pix.setBlue(pix.getRed());
+                }
+            }
+            }
+        }
 
     // Challenge — Write the mirrorDiagonal method that mirrors just a square part of the picture
     // from bottom left to top right around a mirror placed on the diagonal line (the diagonal line
@@ -148,12 +192,17 @@ public class Picture extends SimplePicture
      */
     public static void main(String[] args) {
         Picture pic = new Picture("beach.jpg");
-//        Picture pic = new Picture("water.jpg");
+        Picture pic2 = new Picture("water.jpg");
 
         // The explore method makes a pop-up window of the current picture
 //        pic.explore();
-        pic.zeroBlue();
-        pic.explore();
+//        pic.zeroBlue();
+//        pic.keepOnlyBlue();
+      //  pic.negate();
+//        pic.grayscale();
+        //pic.mirrorCopy();
+        pic2.fixUnderwater();
+        pic2.explore();
     }
 
 } // this } is the end of class Picture, put all new methods before this
